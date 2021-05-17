4e2e9afeac14ffc49f8d7668b1312951d48441cc
Phu Le <phu.le.dev@gmail.com>
2021-05-17 01:27:12 +0000

    MODIFY add create cloudrun service

--- gcp/compute/viet-ocr-stage/main.tf
+++ gcp/compute/viet-ocr-stage/main.tf
@@ -1,8 +1,77 @@
+locals {
+    location = "asia-northeast1"
+    viet_ocr_image = "asia-northeast1-docker.pkg.dev/viet-ocr-stage-8650711b/viet-ocr-stage-repository/viet-ocr"
+    viet_ocr_image_version = "1.0.0"
+}
+
+data "google_iam_policy" "viet_ocr_noauth" {
+    binding {
+        role = "roles/run.invoker"
+        members = [
+            "allUsers",
+        ]
+    }
+}
+
 resource "google_artifact_registry_repository" "viet_ocr_docker_repository" {
     provider = google-beta
-    location = "asia-northeast1"
+    location = local.location
     repository_id   = "viet-ocr-stage-repository"
     description     = "Repository for Viet OCR"
     project = "viet-ocr-stage-8650711b"
     format  = "DOCKER"
+}
+
+resource "google_service_account" "viet_ocr_cloudrun_revision" {
+    account_id   = "viet-ocr-cloudrun-revision-sa"
+    display_name = "Viet OCR Cloud run Revision Service Account"
+}
+
+resource "google_project_iam_member" "viet_ocr_cloudrun_serviceaccount_user" {
+    role    = "roles/iam.serviceAccountUser"
+    member  = "serviceAccount:${google_service_account.viet_ocr_cloudrun_revision.email}"
+}
+
+resource "google_project_iam_member" "viet_ocr_cloudrun_runadmin" {
+    role    = "roles/run.admin"
+    member  = "serviceAccount:${google_service_account.viet_ocr_cloudrun_revision.email}"
+}
+
+resource "google_project_iam_member" "viet_ocr_cloudrun_artifactregistry_reader" {
+    role    = "roles/artifactregistry.reader"
+    member  = "serviceAccount:${google_service_account.viet_ocr_cloudrun_revision.email}"
+}
+
+
+resource "google_cloud_run_service" "viet_ocr" {
+    name     = "viet-ocr-backend-srv"
+    location = local.location
+
+    template {
+        spec {
+            containers {
+                image = "${local.viet_ocr_image}:${local.viet_ocr_image_version}"
+            }
+            service_account_name = google_service_account.viet_ocr_cloudrun_revision.email
+        }
+        metadata {
+            labels = {
+                role = "viet-ocr-backend"
+            }
+        }
+    }
+
+    traffic {
+        percent         = 100
+        latest_revision = true
+    }
+
+    autogenerate_revision_name = true
+}
+
+resource "google_cloud_run_service_iam_policy" "viet_ocr_noauth" {
+  location    = google_cloud_run_service.viet_ocr.location
+  project     = google_cloud_run_service.viet_ocr.project
+  service     = google_cloud_run_service.viet_ocr.name
+  policy_data = data.google_iam_policy.viet_ocr_noauth.policy_data
 }
--- gcp/viet-ocr-stage.mk
+++ gcp/viet-ocr-stage.mk
@@ -5,35 +5,33 @@ VIET_OCR_STAGE_TF_KEY_VERSION	:= 1	# Secret Manager Version
 VIET_OCR_STAGE_TF_SOURCE_DIR	:= /work/compute/viet-ocr-stage
 
 
-.PHONY: gcp-configuration-viet-ocr-stage
-gcp-configuration-viet-ocr-stage:
-	@$(MAKE) gcloud-sdk ARG="config set project $(VIET_OCR_STAGE_PROJECT_ID)"
+.PHONY: gcloud-authenticate-$(VIET_OCR_STAGE_PROJECT_NAME)
+gcloud-authenticate-$(VIET_OCR_STAGE_PROJECT_NAME):
+	@$(MAKE) gcloud-authenticate \
+		CONFIGURATION_NAME=$(VIET_OCR_STAGE_PROJECT_NAME) \
+		PROJECT_ID=$(VIET_OCR_STAGE_PROJECT_ID)
+	@$(MAKE) gcloud-sdk ARG="secrets versions access $(VIET_OCR_STAGE_TF_KEY_VERSION) --secret=$(VIET_OCR_STAGE_TF_KEY)" > ./miscs/gcp/credentials/$(addsuffix .json, $(VIET_OCR_STAGE_TF_KEY))
 
 
-.PHONY: gcloud-authenticate-viet-ocr-stage
-gcloud-authenticate-viet-ocr-stage:
-	@$(MAKE) gcloud-sdk ARG="config configurations create $(VIET_OCR_STAGE_PROJECT_NAME)"
-	@$(MAKE) gcloud-sdk ARG="auth login"
-	@$(MAKE) gcp-configuration-viet-ocr-stage
-	@$(MAKE) gcloud-sdk ARG="secrets versions access $(VIET_OCR_STAGE_TF_KEY_VERSION) --secret=$(VIET_OCR_STAGE_TF_KEY)" > ./miscs/gcp/credentials/$(VIET_OCR_STAGE_TF_KEY).json
-
-
-.PHONY: gcp-tf-viet-ocr-stage-init
-gcp-tf-viet-ocr-stage-init: gcp-configuration-viet-ocr-stage
+.PHONY: gcp-tf-$(VIET_OCR_STAGE_PROJECT_NAME)-init
+gcp-tf-$(VIET_OCR_STAGE_PROJECT_NAME)-init:
+	@$(MAKE) gcloud-activate-configuration PROJECT_ID=$(VIET_OCR_STAGE_PROJECT_ID)
 	@$(MAKE) gcloud-terraform-sdk \
 		ARG="-chdir=$(VIET_OCR_STAGE_TF_SOURCE_DIR) init" \
 		CREDENTIAL_FILE=$(addsuffix .json, $(VIET_OCR_STAGE_TF_KEY))
 
 
-.PHONY: gcp-tf-viet-ocr-stage-plan
-gcp-tf-viet-ocr-stage-plan: gcp-configuration-viet-ocr-stage
+.PHONY: gcp-tf-$(VIET_OCR_STAGE_PROJECT_NAME)-plan
+gcp-tf-$(VIET_OCR_STAGE_PROJECT_NAME)-plan:
+	@$(MAKE) gcloud-activate-configuration PROJECT_ID=$(VIET_OCR_STAGE_PROJECT_ID)
 	@$(MAKE) gcloud-terraform-sdk \
 		ARG="-chdir=$(VIET_OCR_STAGE_TF_SOURCE_DIR) plan" \
 		CREDENTIAL_FILE=$(addsuffix .json, $(VIET_OCR_STAGE_TF_KEY))
 
 
-.PHONY: gcp-tf-viet-ocr-stage-apply
-gcp-tf-viet-ocr-stage-apply: gcp-configuration-viet-ocr-stage
+.PHONY: gcp-tf-$(VIET_OCR_STAGE_PROJECT_NAME)-apply
+gcp-tf-$(VIET_OCR_STAGE_PROJECT_NAME)-apply:
+	@$(MAKE) gcloud-activate-configuration PROJECT_ID=$(VIET_OCR_STAGE_PROJECT_ID)
 	@$(MAKE) gcloud-terraform-sdk \
 		ARG="-chdir=$(VIET_OCR_STAGE_TF_SOURCE_DIR) apply" \
 		CREDENTIAL_FILE=$(addsuffix .json, $(VIET_OCR_STAGE_TF_KEY))
